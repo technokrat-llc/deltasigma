486fab649ec5a833101de7eebf5b3bf3618a80b2
Noah Hüsser <yatekii@yatekii.ch>
2019-10-18 01:22:55 +0200

    JNI with Gecko works

---
+++ DeltaSigma.java
@@ -0,0 +1,12 @@
+class DeltaSigma {
+    // This declares that the static `hello` method will be provided
+    // a native library.
+    public static native String hello(String input);
+
+    static {
+        // This actually loads the shared object that we'll be creating.
+        // The actual location of the .so or .dll may differ based on your
+        // platform.
+        System.loadLibrary("deltasigma");
+    }
+}
